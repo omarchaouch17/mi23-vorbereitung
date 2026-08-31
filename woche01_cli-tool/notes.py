f3fdf6befedb1af115d7b63f8d4aded2e449dd1a
Omar <amrouchchaouch@gmail.com>
2026-08-31 13:44:30 +0200

    add_note, save, load implementiert und getestet

--- woche01_cli-tool/notes.py
+++ woche01_cli-tool/notes.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from datetime import datetime
 
 @dataclass
@@ -11,3 +11,37 @@ class Note:
     def __post_init__(self):
         if self.erstellt_am is None:
             self.erstellt_am = datetime.now().isoformat()
+
+import json
+from pathlib import Path
+
+
+class NoteManager:
+    def __init__(self, filepath="notes_data.json"):
+        self.filepath = Path(filepath)
+        self.notes = []
+        self.load()
+
+    def add_note(self, titel, inhalt):
+        neue_id = len(self.notes) + 1
+        neue_notiz = Note(neue_id, titel, inhalt)
+        self.notes.append(neue_notiz)
+        self.save()
+
+    def save(self):
+        notes_als_dicts = []
+        for notiz in self.notes:
+            notes_als_dicts.append(asdict(notiz))
+
+        with open(self.filepath, "w") as f:
+            json.dump(notes_als_dicts, f)
+
+    def load(self):
+        if self.filepath.exists():
+            with open(self.filepath, "r") as f:
+                geladene_dicts = json.load(f)
+
+            for d in geladene_dicts:
+                self.notes.append(Note(**d))
+        else:
+            self.notes = []
